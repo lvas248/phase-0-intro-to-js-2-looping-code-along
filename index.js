54fdde791712c30c1e82c951652f5ffeaed036a7
Luis <lvas248@gmail.com>
2022-05-20 16:32:26 -0400

    commit tree d1320f3a7be0129a5fca88620125a1dda89d3d01

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, event){
+    const newArray = [];
+    for(let i = 0; i < arrayOfNames.length; i++){
+        const message = `Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`;
+        newArray[i]= message;
+        console.log(message);
+    }
+    return newArray;
+};
+
+function countDown(int){
+    while(int >= 0){
+        console.log(int);
+        int--;
+    }
+}
